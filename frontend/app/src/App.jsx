bd6f2ef81202b43fe09f4cc5c7490250e14bdc71
Olha Prekrasna <prekrasnaya.olya@gmail.com>
2025-10-01 10:17:35 +0200

    frontend fixed

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -4,7 +4,6 @@ import SignUpForm from './components/ui/SignUpForm/SignUpForm.jsx';
 export default function App() {
   return (
     <div>
-      <h1>Welcome to ICHgramm</h1>
       <SignUpForm />
     </div>
   );
--- frontend/app/src/main.jsx
+++ frontend/app/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import { Provider } from "react-redux";
-// import store from "./redux/store";
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </Provider>
+    </BrowserRouter>
   </StrictMode>
 );
+
